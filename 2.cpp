87f2917cab792d305934fcd45924f8c07c74de64
NaitikLakum <naitiklakum6@gmail.com>
2025-07-28 17:47:55 +0530

    Create 2.cpp

---
+++ 2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    double A, B;
+
+    cout << "Enter first number: ";
+    cin >> A;
+
+    cout << "Enter second number: ";
+    cin >> B;
+
+    cout << "Difference: " << A - B << endl;
+
+
+    return 0;
+}
